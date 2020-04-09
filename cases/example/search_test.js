e691ee5134a66905e0c5f0ce8d70e0aeb93be548
Elizaveta Prokhorova <lisa-bella97@mail.ru>
2020-04-09 21:17:18 +0300

    Refactor code

--- cases/example/search_test.js
+++ cases/example/search_test.js
@@ -1,7 +1,7 @@
 import search from '../../steps/search';
 
 describe('test id', () => {
-		it('Найти фильмы по жанру', () => {
+	it('Найти фильмы по жанру', () => {
 		search.open('https://cinsear.ru/search');
 		search.searchByGenre('боевик');
 	});
