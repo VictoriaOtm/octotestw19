5d282c6da6a3ace0f870a7523e101d09e6d06c4a
Nicholas Eskov <mr.eskov1@yandex.ru>
2020-10-25 17:33:12 +0300

    tutor fixes#1

---
+++ cases/example/cloud_recomendation_isq_test .js
@@ -0,0 +1,13 @@
+import account from '../../steps/account';
+import cloudRecommend from '../../steps/cloud_recommend';
+
+describe('cloud_recommendations', () => {
+    before(() => {
+		account.auth();
+		cloudRecommend.openCloudRecommendations();
+    });
+
+	it('Авторизованным попробовать перейти по рекомендации ISQ', () => {
+		cloudRecommend.checkIsq();
+	});
+});
--- cases/example/cloud_recomendation_screnshoter_test.js
+++ cases/example/cloud_recomendation_screnshoter_test.js
@@ -2,20 +2,10 @@ import account from '../../steps/account';
 import cloudRecommend from '../../steps/cloud_recommend';
 
 describe('cloud_recommendations', () => {
-
     before(() => {
 		account.auth();
 		cloudRecommend.openCloudRecommendations();
-
     });
-
-	it('Авторизованным попробовать перейти по рекомендации ISQ', () => {
-		cloudRecommend.checkIsq();
-	});
-
-	it('Авторизованным попробовать перейти по рекомендации Disk-O', () => {
-		cloudRecommend.checkDiskO();
-	});
 
 	it('Авторизованным попробовать перейти по рекомендации Screenshoter', () => {
 		cloudRecommend.checkclickScreenshoter();
